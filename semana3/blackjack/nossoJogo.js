653269bb9fee183837b7cdcb502ad803fe70fc2a
Lucas Alencar Miranda <lucasa.miranda@hotmail.com>
2020-06-05 15:53:07 -0300

    Modificação no código no nossoJogo.js

--- semana3/blackjack/nossoJogo.js
+++ semana3/blackjack/nossoJogo.js
@@ -11,11 +11,25 @@
  * 
  */
 
+
+let jogar = false;
+do {
+
+   if(confirm("pergunta de sim ou não")) {
+      //o que fazer se o usuário clicar "OK"
+      jogar = true;
+   } else {
+      // o que fazer se o usuário clicar "cancelar"
+      jogar = false;
+   }
+
 if("Iniciar o jogo BlackJack"){
    carta1 = comprarCarta();
    carta2 = comprarCarta();
    carta3 = comprarCarta();
    carta4 = comprarCarta();
+
+   
 
    console.log("Usuário - cartas:", carta1.texto, carta2.texto, "Pontuação: ", carta1.valor + carta2.valor );
    console.log("computador - cartas: ", carta3.texto, carta4.texto, "Pontuação: ", carta3.valor + carta4.valor);
@@ -29,8 +43,11 @@ if("Iniciar o jogo BlackJack"){
    }else if((carta1.valor) + (carta2.valor) < (carta3.valor) + (carta4.valor)){
       console.log("O computador Ganhou :(" );
    }
+
    
 }
 else{
    console.log("O jogo acabou!");
 }
+
+}while(jogar != false);

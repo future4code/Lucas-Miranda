6e4ef75be088769b50061dcf5a48501a5d51d69b
Lucas Alencar Miranda <lucasa.miranda@hotmail.com>
2020-06-12 13:16:11 -0300

    Novo commit

--- semana4/funcoes-de-array/controlador-de-despesas/main.js
+++ semana4/funcoes-de-array/controlador-de-despesas/main.js
@@ -1,12 +1,19 @@
+let valor =  document.getElementById("valor"); 
+let description = document.getElementById("description");
+let tipo = document.getElementById("options");
+let listaDespesa = document.getElementById("listaDespesa"); 
+let valorMax = document.getElementById("maximo");
+let valorMin = document.getElementById("minimo");
+let tipo2 = document.getElementById("option2");
+let arrayDespesa = []
+let despesa;
+let listaFiltrada = document.getElementById("listaFiltrar")
+
 function limparCadastro(){
-    const valorMax = document.getElementById("maximo");
-    const valorMin = document.getElementById("minimo");
-    let valor =  document.getElementById("valor"); 
-    let description = document.getElementById("description");
     valor.value=""
-    description.value=""
     valorMax.value =""
     valorMin.value=""
+    description.value=""
     
 }
 
@@ -18,44 +25,26 @@ function validarCadastro(){
     
         alert("Preencha o campo, porfavor!!!")
 
-        const despesa={
-            valor: valor.value,
-            description: description.value,
-            tipo : tipo.value
-
-        }
         arrayDespesa.push(despesa)
         console.log(arrayDespesa);
              
     }else{
-      mostrarDespesas()
+       despesa={
+        valor: valor.value,
+        description: description.value,
+        tipo : tipo.value
+
     }
+    arrayDespesa.push(despesa)
+    mostrarDespesas()
+    console.log(arrayDespesa);
+    }
+
 }
 
-let arrayDespesa = [
-    
-]
 
 function mostrarDespesas(){
-    let valor =  document.getElementById("valor"); 
-    let description = document.getElementById("description");
-    let tipo = document.getElementById("options");
-    let listaDespesa = document.getElementById("listaDespesa"); 
 
-    let filterDespesas =  arrayDespesa.filter((despesa, idx, arr ) =>{
-        return despesa.tipo === tipo.value
-        
-    })
-    filterDespesas =  arrayDespesa.filter((despesa, idx, arr ) =>{
-        return despesa.description === description.value
-        
-    })
-    filterDespesas =  arrayDespesa.filter((despesa, idx, arr ) =>{
-        return despesa.valor === valor.value
-        
-    })
-    console.log(filterDespesas);
-   
         
     listaDespesa.innerHTML += `
     <p>valor: ${valor.value}</p>
@@ -63,3 +52,22 @@ function mostrarDespesas(){
     <p>Tipo:${tipo.value}</p>
     `
 }
+function listaFiltrar(){
+    let filterDespesas =  arrayDespesa.filter((despesa, idx, arr ) =>{
+       return (despesa.tipo === tipo2.value) && (Number(despesa.valor) >= Number(valorMin.value)) && (Number(despesa.valor) <= Number(valorMax.value))
+        
+    })
+    console.log(despesa.valor)
+    console.log(valorMin.value)
+    console.log(filterDespesas)
+    console.log(arrayDespesa)
+
+    filterDespesas.forEach((despesa, idx, arr) =>{
+        listaFiltrada.innerHTML += `
+        <p>valor: ${despesa.tipo}</p>
+        <p>Tipo:${despesa.description}<p>
+        <p>Descrição:${despesa.valor}</p>
+         `
+    }) 
+
+}    

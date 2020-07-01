7e0adfcce9e783afd075cb86bc191cbcc5bc2a02
Lucas Alencar Miranda <lucasa.miranda@hotmail.com>
2020-07-01 17:37:46 -0300

    Código corrigido e funcional aula25

--- LabUsers/lab_user/src/App.js
+++ LabUsers/lab_user/src/App.js
@@ -1,27 +1,28 @@
 import React from 'react';
-import axios from "axios";
-import logo from './logo.svg';
 import './App.css';
-import {CadUser} from './components/CadUser';
-import {ListaUser} from './components/ListaUser';
+import styled from 'styled-components'
+import SignUpPage  from './components/SignUpPage';
+import UserListPage from './components/UserListPage';
+
+const AppContainer = styled.div`
+  font-family: sans-serif;
+  text-align: center;
+`;
 
 
 class App extends React.Component{
 
   state = {
-    name: "",
-    email: ""
+    currentPage: "SignUpPage"
   };
 
- componentDidMount =()=>{
-   this.getAllUsers();
- };
-
- criarNovoUser = () =>{
-  const body = {
-  name: this.state.name
-  };
- }
+  onChangePage = () =>{
+    if (this.state.currentPage === "SignUpPage"){
+      this.setState({currentPage: "UserListPage"});
+    }else{
+      this.setState({currentPage: "SignUpPage"})
+    }
+  }
 
  onchangeName = (e) =>{
    this.setState({name: e.target.value})
@@ -31,53 +32,16 @@ class App extends React.Component{
    this.setState({email: e.target.value})
  }
 
- getAllUsers = () =>{
-   axios
-    .get(
-      "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users", body,
-     {
-        headers: {
-          Authorization: "Lucas-Alencar-Turing"
-        }
-     }
-    )
-   .then(response =>{
-     this.setState({name: response.data.result.name})
-     this.setState({email: response.data.result.email})
-   })
-   .catch(error => {
-     window.alert("Não foi possível cadastrar usuário. =(")
-     console.log(error.data);
-   });
- };
-
- DeleteAllUser = () =>{
- 
-    axios
-     .del(
-       "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/:id", body,
-      {
-         headers: {
-           Authorization: "Lucas-Alencar-Turing"
-         }
-      }
-     )
-    .then(response =>{
-      this.setState({name: response.data.result.name})
-      this.setState({email: response.data.result.email})
-    })
-    .catch(error => {
-      console.log(error.data);
-    });
-  };
- 
-
  render(){
+   const page = this.state.currentPage === "SignUpPage" ? (<SignUpPage/>) : (<UserListPage/>);
+   
+   const buttonName = this.state.currentPage === "SignUpPage" ? "Ir para lista" : "Ir para cadastro";
+
    return(
-     <div>
-       <CadUser />
-       <ListaUser />
-     </div>
+    <AppContainer>
+      <button onClick={this.onChangePage}>{buttonName}</button>
+      {page}
+    </AppContainer>
    )
  }
 
